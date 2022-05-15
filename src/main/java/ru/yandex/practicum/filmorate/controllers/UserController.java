c12068c9026610864652065e8662f7ce863193ac
Rinat <eenot84@yandex.ru>
2022-05-15 10:28:33 +0300

    Sprint8

--- src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
@@ -59,23 +59,19 @@ public class UserController {
 //        }
 //    }
 
-    public void validate(User user) {
+    public void validate(User user)  {
         if (user.getLogin() == null || user.getLogin().isBlank() || user.getLogin().contains(" ")) {
             log.error("Логин {} содержит пробелы или пустой", user.getLogin());
             throw new ValidationException("Логин содержит пробелы или пустой");
         }
-
-        //Используем логин если имя пустое
         if (user.getName().isEmpty()) {
             user.setName(user.getLogin());
         }
-
         if (user.getEmail() == null || user.getEmail().isBlank() || !user.getEmail().contains("@")) {
             log.error("Ошибка формата email '{}'", user.getEmail());
             throw new ValidationException("Ошибка формата email");
         }
-
-        if (user.getBirthday().isAfter(LocalDate.now())) {
+        if (user.getBirthday().isBefore(LocalDate.now())) {
             log.error("Ошибка даты рождения '{}'", user.getBirthday());
             throw new ValidationException("Ошибка в поле дата рождения");
         }
