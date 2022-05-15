ede65165a77440408f2f05aae7a4956549c8c720
Rinat <eenot84@yandex.ru>
2022-05-15 10:46:29 +0300

    Sprint8

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -3,6 +3,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 @Data
 @AllArgsConstructor
@@ -11,6 +12,7 @@ public class User {
     private int id;
     @Email
     private String email;
+    @Size(max = 30, message = "Имя может быть не больше тридцати символов")
     private String name;
     private String login;
     private LocalDate birthday;
