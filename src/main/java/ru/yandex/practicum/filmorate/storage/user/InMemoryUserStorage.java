0f0427a37842d89b1fd410b1ea495d6d16e24a52
Rinat <eenot84@yandex.ru>
2022-05-22 22:38:14 +0300

    Sprint 9

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/InMemoryUserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/InMemoryUserStorage.java
@@ -18,30 +18,28 @@ public class InMemoryUserStorage implements UserStorage {
 
     @Override
     public User createUser(User user) throws ValidationException {
-        if(checkValidUser(user)) {
+        validate(user);
             user.setId(id);
             users.put(id, user);
             id++;
             log.info("Пользователь {} добавлен", user);
-        }
         return user;
     }
 
     @Override
     public User updateUser(User user) throws ValidationException {
+        validate(user);
         if (!users.containsKey(user.getId())) {
             throw new ValidationException("пользователь " + user.getId());
         }
         User updateUser = users.get(user.getId());
-        if(checkValidUser(user)) {
             updateUser.setEmail(user.getEmail());
             updateUser.setLogin(user.getLogin());
             updateUser.setBirthday(user.getBirthday());
             updateUser.setName(user.getName());
             users.put(user.getId(), updateUser);
             log.info("Данные о пользователе {} обновлены", updateUser);
-        }
-        return null;
+            return user;
     }
 
     @Override
@@ -71,22 +69,40 @@ public class InMemoryUserStorage implements UserStorage {
         return users.get(id);
     }
 
-    public boolean checkValidUser(User user) {
-        if (user.getEmail().isBlank() || !user.getEmail().contains("@")) {
-            log.error("Введен пустой email или отсутствует символ @.");
-            throw new ValidationException("Email не может быть пустым и должен содержать символ @.");
+
+    public void validate(User user) {
+        if (user.getLogin() == null || user.getLogin().isBlank() || user.getLogin().contains(" ")) {
+            log.error("Логин {} содержит пробелы или пустой", user.getLogin());
+            throw new ValidationException("Логин содержит пробелы или пустой");
         }
-        if (user.getLogin().isBlank() || user.getLogin().contains(" ")) {
-            log.error("Введен пустой логин или логин содержит пробелы.");
-            throw new ValidationException("Логин не может быть пустым или содержать пробелы.");
-        }
-        if (user.getName().isBlank() || user.getName() == null) {
+        if (user.getName().isEmpty()) {
             user.setName(user.getLogin());
         }
-        if (user.getBirthday().isAfter(LocalDate.now())) {
-            log.error("Введена дата рождения из будущего.");
-            throw new ValidationException("Дата рождения не может быть в будущем.");
+        if (user.getEmail() == null || user.getEmail().isBlank() || !user.getEmail().contains("@")) {
+            log.error("Ошибка  email '{}'", user.getEmail());
+            throw new ValidationException("Ошибка формата email");
         }
-        return true;
-    }
+        if (user.getBirthday().isAfter(LocalDate.now())) {
+            log.error("Ошибка даты рождения '{}'", user.getBirthday());
+            throw new ValidationException("Ошибка в поле дата рождения");
+        }
+
+//    public boolean checkValidUser(User user) {
+//        if (user.getEmail().isBlank() || !user.getEmail().contains("@")) {
+//            log.error("Введен пустой email или отсутствует символ @.");
+//            throw new ValidationException("Email не может быть пустым и должен содержать символ @.");
+//        }
+//        if (user.getLogin().isBlank() || user.getLogin().contains(" ")) {
+//            log.error("Введен пустой логин или логин содержит пробелы.");
+//            throw new ValidationException("Логин не может быть пустым или содержать пробелы.");
+//        }
+//        if (user.getName().isBlank() || user.getName() == null) {
+//            user.setName(user.getLogin());
+//        }
+//        if (user.getBirthday().isAfter(LocalDate.now())) {
+//            log.error("Введена дата рождения из будущего.");
+//            throw new ValidationException("Дата рождения не может быть в будущем.");
+//        }
+//        return true;
+//    }
 }
